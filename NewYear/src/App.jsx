f66b13787d7a488a5a172fc4cadc59c8ae540be9
DanizThere <bespalov.bespalov2000@gmail.com>
2024-11-25 15:12:06 +0500

    MainContainers

--- NewYear/src/App.jsx
+++ NewYear/src/App.jsx
@@ -1,4 +1,28 @@
 import './App.css'
 
 export default function App() {
+    return(
+        <>
+            <main>
+                <div className='container'>
+                    <div className='mainText'>
+                        <h1>Кафедра ИСТ</h1>
+                    </div>
+                    <div className='gifBobots'>
+
+                    </div>
+                </div>
+            </main>
+            <section>
+                <div className='container'>
+
+                </div>
+            </section>
+            <section>
+                <div className='container'>
+                    
+                </div>
+            </section>
+        </>
+    )
 }
