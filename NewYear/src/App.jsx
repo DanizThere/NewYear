7e48428b3a84ed36f75924d5477d82b91c8d564b
DanizThere <bespalov.bespalov2000@gmail.com>
2024-11-28 17:09:38 +0500

    Navigation

--- NewYear/src/App.jsx
+++ NewYear/src/App.jsx
@@ -1,26 +1,43 @@
 import './App.css'
+import logo from './assets/cropped-logo.png'
 
 export default function App() {
     return(
         <>
-            <main>
+            <header>
+                <div className='container'>
+                    <div className='img'>
+                    <a href='https://ist.uisi.ru/' title='На кафедру ИСТ'><img src={logo}></img></a>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href='#first'>Начало</a></li>
+                            <li><a href='#second'>Описание</a></li>
+                            <li><a href='#third'>Фотография</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+            <main className='first' id='first'>
                 <div className='container'>
                     <div className='mainText'>
-                        <h1>Кафедра ИСТ</h1>
+                        <h1>Боботы</h1>
                     </div>
                     <div className='gifBobots'>
 
                     </div>
                 </div>
             </main>
-            <section>
+            <section className='second' id='second'>
                 <div className='container'>
 
                 </div>
             </section>
-            <section>
+            <section className='third' id='third'>
                 <div className='container'>
-                    
+                    <select>
+                        <option></option>
+                    </select>
                 </div>
             </section>
         </>
